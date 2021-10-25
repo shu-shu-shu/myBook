b4971ea68bc0c538de203ab76caccb81d63db165
shu-shu-shu <shuhei.tta@gmail.com>
2021-10-25 20:22:13 +0900

    first commit

---
+++ blog/views.py
@@ -0,0 +1,92 @@
+# blog/views.py
+
+from django.db.models import Count, Q
+from django.http import Http404
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from blog.models import Post, Category, Tag
+from django.shortcuts import get_object_or_404
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        if not obj.is_public and not self.request.user.is_authenticated:
+            raise Http404
+        return obj
+
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    paginate_by = 3
+
+
+class CategoryListView(ListView):
+    queryset = Category.objects.annotate(
+        num_posts=Count('post', filter=Q(post__is_public=True)))
+
+
+class TagListView(ListView):
+    queryset = Tag.objects.annotate(num_posts=Count(
+        'post', filter=Q(post__is_public=True)))
+
+class CategoryPostView(ListView):
+    model = Post
+    template_name = 'blog/category_post.html'
+
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        self.category = get_object_or_404(Category, slug=category_slug)
+        qs = super().get_queryset().filter(category=self.category)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
+
+
+class TagPostView(ListView):
+    model = Post
+    template_name = 'blog/tag_post.html'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug']
+        self.tag = get_object_or_404(Tag, slug=tag_slug)
+        qs = super().get_queryset().filter(tags=self.tag)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
+
+
+class SearchPostView(ListView):
+    model = Post
+    template_name = 'blog/search_post.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        query = self.request.GET.get('q', None)
+        lookups = (
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(category__name__icontains=query) |
+            Q(tags__name__icontains=query)
+        )
+        if query is not None:
+            qs = super().get_queryset().filter(lookups).distinct()
+            return qs
+        qs = super().get_queryset()
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        return context
